45be78ccef4f990d24b5d7d4011f7cfc40fbeffe
vsm34 <varunmantha@yahoo.com>
2025-11-06 16:59:48 -0500

    Update DBConnection.java

--- BuyMe_Login/src/utils/DBConnection.java
+++ BuyMe_Login/src/utils/DBConnection.java
@@ -5,8 +5,8 @@ import java.sql.DriverManager;
 
 public class DBConnection {
     private static final String URL  = "jdbc:mysql://localhost:3306/buyme?useSSL=false&allowPublicKeyRetrieval=true";
-    private static final String USER = "root";
-    private static final String PASS = "Srisai12"; // replace locally
+    private static final String USER = "<yourusername>";
+    private static final String PASS = "<yourpassword>"; // replace locally
 
     static {
         try { Class.forName("com.mysql.cj.jdbc.Driver"); }
